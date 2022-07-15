10410075df28787d36e29bab64b241d3cc2e9d83
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2022-07-15 09:49:20 +0200

    will

--- functions/cheerio/cheerio.js
+++ functions/cheerio/cheerio.js
@@ -2,14 +2,14 @@ const request = require('axios')
 const cheerio = require('cheerio')
 
 exports.handler = async (event, context) => {
-  const url = 'https://whatever.com'
+  const url = 'https://www.willhaben.at/iad/immobilien/mietwohnungen/oberoesterreich/linz'
   try {
     const { data } = await request(url)
     const $ = cheerio.load(data)
     /* queryDOM */
-    const items = $('#idXyz li')
-    console.log('items', items)
-    const itemsText = items.map((i, el) => {
+    const resultTitle = $('##result-list-title')
+    console.log('result title', resultTitle)
+    /* const itemsText = items.map((i, el) => {
       return $(el).text().trim()
     })
     console.log('itemsText', items)
@@ -18,7 +18,7 @@ exports.handler = async (event, context) => {
       body: JSON.stringify({
         items: itemsText,
       })
-    }
+    } */
   } catch (error) {
     return {
       statusCode: 500,
