991f27948114b2ed245aa671917220d42684724b
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2022-07-15 15:18:16 +0200

    -

--- functions/playwright/playwright.js
+++ functions/playwright/playwright.js
@@ -5,12 +5,12 @@ exports.handler = async (event, context) => {
   let browser = null;
   console.log("spawning chrome headless");
   console.log(await chromium.executablePath());
-  console.log(process.env);
+  // console.log(process.env);
 
   try {
     browser = await chromium.launch({
       headless: true, // Show the browser.
-      executablePath: '/opt/build/repo/node_modules/chromium/lib/chromium/chrome-linux/chrome',
+      // executablePath: '/opt/build/repo/node_modules/chromium/lib/chromium/chrome-linux/chrome',
     });
     console.log(chromium.executablePath());
     const page = await browser.newPage();
